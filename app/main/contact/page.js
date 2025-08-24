1b387a1baa24f2812ffe6c4e3f2f1ff655cd8467
cepiyogaasmara <cepiyoga@gmail.com>
2025-08-24 23:13:39 +0700

    Update Sekali

--- app/main/contact/page.js
+++ app/main/contact/page.js
@@ -1,37 +1,7 @@
-export const metadata = {
-    title: "Contact",
-}
-
-export default async function Contact() {
-    const response = await fetch("http://localhost:3001/posts");
-    const posts = await response.json();
-    console.log(posts);
+export default function ContactPage() {
     return (
         <>
-            <div>
-                <div>Contact</div>
-                <table>
-                    <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>title</th>
-                        <th>view</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {posts.map((post, index) => (
-                        <tr key={index}>
-                            <td>{post.id}</td>
-                            <td>{post.title}</td>
-                            <td>{post.views}</td>
-
-                        </tr>
-
-                    ))}
-                    </tbody>
-                </table>
-
-            </div>
+            <h1>Contact</h1>
         </>
     )
 }

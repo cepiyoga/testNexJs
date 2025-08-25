434cf2791ae7a57eaadc16d0f7e55c6ed9262919
cepiyogaasmara <cepiyoga@gmail.com>
2025-08-25 11:00:14 +0700

    update 43A

--- app/main/home/page.js
+++ app/main/home/page.js
@@ -6,7 +6,7 @@ export const metadata = {
 export default function Home() {
     return (
         <>
-            <h1 className={`text-4xl bg-green-500`}>Home Baru</h1>
+            <h1 className={`text-4xl bg-green-500 my-2 rounded px-2 py-1 text-white`}>Home Baru</h1>
             <div className="flex gap-x-2">
                 <Link className={`bg-gray-400 px-3 rounded py-1`} href='/blog/bcd'>Blog</Link>
                 <Link className={`bg-sky-400 px-3 rounded py-1`} href="/blog/bbg">OK</Link>

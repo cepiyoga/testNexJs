36e8973432f702abe850dc3ebbe3ef9734ee0ee9
cepiyogaasmara <cepiyoga@gmail.com>
2025-08-30 10:55:07 +0700

    update 30 Aug

--- app/main/home/page.js
+++ app/main/home/page.js
@@ -6,7 +6,35 @@ export const metadata = {
 export default function Home() {
     return (
         <>
-            <h1 className={`text-4xl bg-green-500 my-2 rounded px-2 py-1 text-white`}>Home Baru</h1>
+            <h1 className={`text-4xl bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Home Baru</h1>
+
+            <div className={`flex justify-between`}>
+                <div className={`space-x-4 bg-amber-200`}>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                </div>
+                <div className={`space-x-4 bg-amber-200`}>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+                </div>
+                <div className={`space-x-4 bg-amber-200`}>
+                    <button className={` bg-green-500 my-2 rounded px-2 py-1 text-white text-center`}>Ok</button>
+
+                </div>
+
+            </div>
+
             <div className="flex gap-x-2">
                 <Link className={`bg-gray-400 px-3 rounded py-1`} href='/blog/bcd'>Blog</Link>
                 <Link className={`bg-sky-400 px-3 rounded py-1`} href="/blog/bbg">OK</Link>
--- componensts/nav_main.js
+++ componensts/nav_main.js
@@ -1,18 +1,33 @@
+'use client'
+
+
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 
 export default function NavMain() {
+
+
+    let pathname = usePathname();
+    console.log(pathname);
+
     return (
         <>
-            <div className="bg-gray-100 h-20 flex items-center">
-                <ul className={`flex gap-2 p-2 font-semibold`}>
-                    <li><Link href="/">Root</Link></li>
-                    <li><Link href="/main/home">Home</Link></li>
-                    <li><Link href="/main/about">About</Link></li>
-                    <li><Link href="/main/contact">Contact</Link></li>
-                    <li><Link href="/main/data">Data</Link></li>
-                    <li><Link href="/main/toggle">Toggle</Link></li>
-                    <li><Link href="/graph">Graph</Link></li>
-                </ul>
+            <div className="bg-gray-100 h-20 flex items-center gap-2 text-xl">
+                <Link href="/"
+                      className={`${pathname === '/' ? 'font-bold' : ''}`}>Root</Link>
+                <Link href="/main/home"
+                      className={`${pathname === '/main/home' ? 'font-bold animate-pulse' : ''}`}>Home</Link>
+                <Link href="/main/about"
+                      className={`${pathname === '/main/about' ? 'font-bold animate-pulse' : ''}`}>About</Link>
+                <Link href="/main/contact"
+                      className={`${pathname === '/main/contact' ? 'font-bold animate-pulse' : ''}`}>Contact</Link>
+                <Link href="/main/data"
+                      className={`${pathname === '/main/data' ? 'font-bold animate-pulse' : ''}`}>Data</Link>
+                <Link href="/main/toggle"
+                      className={`${pathname === '/main/toggle' ? 'font-bold animate-pulse' : ''}`}>Toggle</Link>
+                <Link href="/graph"
+                      className={`${pathname === '/graph' ? 'font-bold animate-pulse' : ''}`}>Graph</Link>
+
 
             </div>
 
